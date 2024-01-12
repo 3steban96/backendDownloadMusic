00fb9274049d0bf774a3da8c58482e0201cc3908
3steban96 <114366197+3steban96@users.noreply.github.com>
2024-01-11 20:13:52 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
 const express = require('express');
-const cors = require('cors'); // Importa la librería cors
+const cors = require('cors');
 const route = require('./route');
 const app = express();
 const port = 3000;
-app.use(express.json());
 
+app.use(cors({
+  origin: 'https://www.downloadmusic.cloud',
+  credentials: true,
+  methods: 'GET, POST, OPTIONS, PUT, DELETE, PATCH',
+  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, x-auth-token',
+}));
+
+app.use(express.json());
 app.use('/', route);
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://www.downloadmusic.cloud");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "x-auth-token, Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, DELETE, PATCH"
-  );
-  next();
-});
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
