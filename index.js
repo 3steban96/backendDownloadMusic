85375df5f06a31e54bb36db21147edb1a41fa863
3steban96 <andersonrodriguez748@gmail.com>
2024-01-17 18:09:13 -0500

    update functions

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 
+const { promisify } = require('util');
+const pipeline = promisify(require('stream').pipeline);
+
 const downloadAudio = async (url, audioOptions, response) => {
   try {
     const videoInfo = await ytdl.getInfo(url);
@@ -16,11 +19,6 @@ const downloadAudio = async (url, audioOptions, response) => {
     audio.on('data', (chunk) => {
       audioBuffer.push(chunk);
     });
-    
-    audio.on('end', () => {
-      const audioData = Buffer.concat(audioBuffer).toString('base64');
-      response.json({ message: 'Descarga de audio completada.', fileName: `${sanitizedTitle}.mp3`, audioData });
-    });
 
     audio.on('error', (error) => {
       console.error('Error al descargar el audio:', error.message);
@@ -31,54 +29,43 @@ const downloadAudio = async (url, audioOptions, response) => {
       const percent = (downloaded / total) * 100;
       console.log(`Descargando audio: ${percent.toFixed(2)}%`);
     });
+
+    await pipeline(audio, audioWriteStream);
+
+    const audioData = Buffer.concat(audioBuffer).toString('base64');
+    response.json({ message: 'Descarga de audio completada.', fileName: `${sanitizedTitle}.mp3`, audioData });
   } catch (error) {
     console.error('Error al obtener información del video:', error.message);
     response.status(500).json({ error: 'Error al obtener información del video.' });
   }
 };
-const downloadVideo = async (urlMp4, response) => {
+
+
+const downloadVideo = async (urlMp4,response)=>{
   try {
-    const options = {
-      quality: 'highestvideo',
-      filter: 'videoandaudio',
-    };
-    const video = ytdl(urlMp4, options);
-    const videoInfo = await ytdl.getInfo(urlMp4);
-    const videoTitle = videoInfo.videoDetails.title;
-
-    const sanitizedTitle = videoTitle.replace(/[/\\?%*:|"<>]/g, '-');
-    const videoWriteStream = fs.createWriteStream(`${sanitizedTitle}.mp4`);
-
-    video.pipe(videoWriteStream);
-
-    video.on('end', () => {
-      console.log('Descarga completada.');
-
-      // Establecer el encabezado Content-Disposition para sugerir el nombre de archivo al navegador
-      response.attachment(`${sanitizedTitle}.mp4`);
-      response.sendFile(path.resolve(`${sanitizedTitle}.mp4`), (err) => {
-        if (err) {
-          console.error('Error al enviar el archivo al cliente:', err.message);
-          response.status(500).json({ error: 'Error al enviar el archivo al cliente.' });
-        } else {
-          console.log('Archivo enviado al cliente correctamente.');
-        }
+      const options = {
+        quality: 'highestvideo',
+        filter: 'videoandaudio',
+      };
+      const video = ytdl(urlMp4, options);
+      const videoInfo = await ytdl.getInfo(urlMp4);
+      const videoTitle = videoInfo.videoDetails.title;
+  
+      const sanitizedTitle = videoTitle.replace(/[/\\?%*:|"<>]/g, '-');
+      video.pipe(fs.createWriteStream(`${sanitizedTitle}.mp4`));
+    
+      video.on('end', () => {
+        console.log('Descarga completada.');
       });
-    });
-
-    video.on('error', (error) => {
-      console.error('Error al descargar el video:', error.message);
-      response.status(500).json({ error: 'Error al descargar el video.' });
-    });
-
-    video.on('progress', (chunkLength, downloaded, total) => {
-      const percent = (downloaded / total) * 100;
-      console.log(`Descargando: ${percent.toFixed(2)}%`);
-    });
-
-    videoWriteStream.on('finish', () => {
-      console.log('Escritura del archivo de video finalizada.');
-    });
+    
+      video.on('error', (error) => {
+        console.error('Error al descargar el video:', error.message);
+      });
+    
+      video.on('progress', (chunkLength, downloaded, total) => {
+        const percent = (downloaded / total) * 100;
+        console.log(`Descargando: ${percent.toFixed(2)}%`);
+      });
   } catch (error) {
     console.error('Error al obtener información del video:', error.message);
     response.status(500).json({ error: 'Error al obtener información del video.' });
